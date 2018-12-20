4c7c48d7806bf21d882b328402745031067a5609
Eugene <rubel.eugene@gmail.com>
2018-12-21 01:37:04 +0200

    update js

--- main.js
+++ main.js
@@ -10,5 +10,37 @@ button.onclick = function openMenu(){
 		blockMenu.className = 'menu active';
 	}
 }
-
+$('.title').fadeTo(700,1);
+$('.cocacola').click(function(){
+    $('.banka-pepsi,.banka-sprite,.banka-fanta').fadeTo(500,0);
+    $('.banka-cola').fadeTo(500,1);
+    $('.napitki').css({
+        'background':'url(img/bg.png) center center',
+        'background-size':'cover'
+    });
+});
+$('.pepsi').click(function(){
+    $('.banka-cola,.banka-sprite,.banka-fanta').fadeTo(500,0);
+    $('.banka-pepsi').fadeTo(500,1);
+    $('.napitki').css({
+        'background':'url(img/bg3.jpg) center center',
+        'background-size':'cover'
+    });
+});
+$('.sprite').click(function(){
+    $('.banka-cola,.banka-pepsi,.banka-fanta').fadeTo(500,0);
+    $('.banka-sprite').fadeTo(500,1);
+    $('.napitki').css({
+        'background':'url(img/bg2.jpg) center center',
+        'background-size':'cover'
+    });
+});
+$('.fanta').click(function(){
+    $('.banka-cola,.banka-pepsi,.banka-sprite').fadeTo(500,0);
+    $('.banka-fanta').fadeTo(500,1);
+    $('.napitki').css({
+        'background':'url(img/bg4.jpg) center center',
+        'background-size':'cover'
+    });
+});
 
