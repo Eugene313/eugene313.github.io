8a83785510006d79b89dd3559928d3a5b28b6fef
Eugene313 <rubel.eugene@gmail.com>
2018-12-21 15:43:42 +0200

    add main2

--- main.js
+++ main.js
@@ -43,4 +43,11 @@ $('.fanta').click(function(){
         'background-size':'cover'
     });
 });
+$(function(){
+    $(document).mousemove(function(e){
+        $('#cube').css({
+            'transform':'rotateX('+e.pageY+'deg) rotateY('+e.pageX+'deg)'
+        })
+    })
+})
 
