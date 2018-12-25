7d8437138ae61e16845be3f3cd4ebd7d1ca7d63f
Eugene313 <rubel.eugene@gmail.com>
2018-12-25 16:15:02 +0200

    all

--- main.js
+++ main.js
@@ -1,46 +1,12 @@
-var button = document.getElementById('mobile-menu');
-var blockMenu = document.getElementById('menu');
-
-
-
-button.onclick = function openMenu(){
-	if (blockMenu.classList.contains('active')){
-		blockMenu.className = 'menu';
-	} else {
-		blockMenu.className = 'menu active';
-	}
+var startTime = new Date();
+function showElapsedTime() {
+var testSiteUrl = location.href;;
+var testSiteString = String(testSiteUrl).slice(testSiteUrl.indexOf("www"));
+var endTime = new Date();
+var elapsedTime = Number(endTime-startTime);
+var browser=navigator.userAgent;
+var platform=navigator.platform;
+var msgString = "Speed " + Number(elapsedTime/1000) + " second (" + elapsedTime + " ms)<br/>";
+document.getElementById("vremia").innerHTML = msgString;
 }
-$('.title').fadeTo(700,1);
-$('.cocacola').click(function(){
-    $('.banka-pepsi,.banka-sprite,.banka-fanta').fadeTo(500,0);
-    $('.banka-cola').fadeTo(500,1);
-    $('.napitki').css({
-        'background':'url(img/bg.png) center center',
-        'background-size':'cover'
-    });
-});
-$('.pepsi').click(function(){
-    $('.banka-cola,.banka-sprite,.banka-fanta').fadeTo(500,0);
-    $('.banka-pepsi').fadeTo(500,1);
-    $('.napitki').css({
-        'background':'url(img/bg3.jpg) center center',
-        'background-size':'cover'
-    });
-});
-$('.sprite').click(function(){
-    $('.banka-cola,.banka-pepsi,.banka-fanta').fadeTo(500,0);
-    $('.banka-sprite').fadeTo(500,1);
-    $('.napitki').css({
-        'background':'url(img/bg2.jpg) center center',
-        'background-size':'cover'
-    });
-});
-$('.fanta').click(function(){
-    $('.banka-cola,.banka-pepsi,.banka-sprite').fadeTo(500,0);
-    $('.banka-fanta').fadeTo(500,1);
-    $('.napitki').css({
-        'background':'url(img/bg4.jpg) center center',
-        'background-size':'cover'
-    });
-});
-
+onload=function() {showElapsedTime();}
