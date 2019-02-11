5a50048a3c4515676d76a4c65eb3b0a143a66ba2
Eugene313 <rubel.eugene@gmail.com>
2019-02-11 14:45:02 +0200

    update 2.0

--- js/main.js
+++ js/main.js
@@ -76,7 +76,8 @@ if (xhr.status != 200) {
     //console.log(xhr.responseText);
     app._data.object = JSON.parse(xhr.responseText);
     console.log(app._data.object);
-    app._data.totalDay = app._data.object.reduce((acc.val) => acc + val.totalDay,0)
+    app._data.totalDay = app._data.object.reduce((acc.val) => acc + val.totalDay,0);
+    console.log(app._data.totalDay);
 }
 $('.add-data').click(function () {
 $('.form-data').toggleClass("form-data-active");
